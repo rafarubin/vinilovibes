59f871252fbcff133b00f26d3b5af8b3b61069d9
Ralph Coaquira <ralph.coaquira.rubin@gmail.com>
2024-09-03 12:20:30 -0500

    Validaciones de modelos, seed de User y Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_one :purchase
 
   validates :name, :price, :category, :artist, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(name: "Ralph", email: "ralph@gmail.com", password: "123456")
+User.create(name: "Shin", email: "shin@gmail.com", password: "123456")
+User.create(name: "Adriana", email: "adriana@gmail.com", password: "123456")
+User.create(name: "Paul", email: "paul@gmail.com", password: "123456")
+User.create(name: "Enrique", email: "enrique@gmail.com", password: "123456")
+
+
+Product.create(name: "Funny Girl", price: 10, artist: "Dick Curtis", category: "Variado", user_id: 3)
+Product.create(name: "Californication", price: 15, artist: "RHCP", category: "Funk", user_id: 2)
+Product.create(name: "Golden Days", price: 20, category: "Rock", artist: "Maddie", user_id: 1)
+Product.create(name: "Feeling strong", price: 32, category: "Pop", artist: "Taylor Swift", user_id: 4)
+Product.create(name: "Melancholy Hills", price: 45, category: "Alternative rock", artist: "Gorillaz", user_id: 5)
