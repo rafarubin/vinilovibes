11f8bd5716cab663539f75a1b3b28d751029a6a8
Shinryo Okuhama <157136484+5h1nry0@users.noreply.github.com>
2024-09-03 14:38:19 -0500

    Agregar acciones index, new, create, show, edit, update, destroy, product_params, set_product a controlador Products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,30 @@
 class ProductsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def product_params
+  end
+
+  def set_product
+  end
 end
