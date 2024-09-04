13c64711ac6963720ec7272f814f48fe7c37d306
Ralph Coaquira <ralph.coaquira.rubin@gmail.com>
2024-09-04 15:14:30 -0500

    creacion del controller #create

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,12 +5,20 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.new(purchase_params)
+    @purchase.user = current_user
+    @purchase.product = Product.find(params[:product_id])
 
+    if @purchase.save
+      redirect_to product_path(@purchase.product), notice: 'Compra realizada con éxito.'
+    else
+      render :new, alert: 'Error al realizar la compra.', status: :unprocessable_entity
+    end
   end
 
   private
 
   def purchase_params
-
+    params.require(:purchase).permit(:user_id)
   end
 end
