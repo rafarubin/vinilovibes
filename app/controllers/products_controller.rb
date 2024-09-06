2e87be5c29960e931d45596114ba0a1aa762720b
Shinryo Okuhama <157136484+5h1nry0@users.noreply.github.com>
2024-09-06 14:39:18 -0500

    El index ahora muestra tus productos sin el botón de comprar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,12 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:edit, :update, :show, :destroy]
 
   def index
-    if user_signed_in?
-      @user = current_user
-      @products = Product.where("sold = ? AND user_id != ?", false, @user.id)
-    else
-      @products = Product.where(sold: false)
-    end
+    # if user_signed_in?
+    @user = current_user
+    #   @products = Product.where("sold = ? AND user_id != ?", false, @user.id)
+    # else
+    @products = Product.where(sold: false)
+    # end
     if params[:query].present?
       @products = Product.search_product(params[:query])
     end
